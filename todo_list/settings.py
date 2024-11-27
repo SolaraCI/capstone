4df87591912b256aaf4d25378b3562d731f26cbe
Jonathan Coton <fbp3sgxt@students.codeinstitute.net>
2024-11-27 01:18:30 +0000

    deploy ready

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h($gp)k7w*jqhupu)==389lb$&oytxsjtm4(d5gf^6)q7&$o3x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
