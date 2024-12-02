f5da14d933dc2d013ffa57d279e441f3c12b91aa
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-02 03:24:53 +0000

    create list and item models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class List(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="lists"
+    )
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=500)
+    done = models.BooleanField(default=False)
+    parent_list = models.ForeignKey(
+        List, on_delete=models.CASCADE, related_name="items"
+    )
