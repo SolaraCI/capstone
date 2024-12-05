04c3c5e014e97ea707a5c6669d5900a9cc8a2618
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-05 10:10:55 +0000

    add view_list to urlpatterns

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', 
-        views.HomePage.as_view(), name='home'),
+    path("", views.all_lists, name="home"),
+    path("create_list", views.create_list, name="create_list"),
+    path("lists/<list_id>", views.view_list, name="view_list"),
 ]
