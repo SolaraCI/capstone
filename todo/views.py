91551d033571206f2ee3d6b466eb55f87a621493
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-09 15:37:40 +0000

    fix error in create_list, minor refactor

--- todo/views.py
+++ todo/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import List, Item
 from .forms import ListForm, ItemForm
 from django.forms import modelform_factory
-from django.contrib.messages import constants as messages
+from django.contrib import messages
 
 
 # Create your views here.
 
+
 def all_lists(request):
+    """Retrieves all existing lists to display on home page"""
     all_lists = List.objects.all()
 
     context = {
@@ -18,14 +20,13 @@ def all_lists(request):
 
 
 def view_list(request, list_id):
+    """Retrieves selected list and returns it to render"""
     todo_list = get_object_or_404(List, id=list_id)
-
-    
 
     context = {
         'list': todo_list,
         'items': todo_list.items,
-        'item_names': todo_list.items.values_list("name"),
+        'item_names': todo_list.items.values('name'),
     }
 
     return render(request, 'todo/view_list.html', context)
@@ -37,11 +38,12 @@ def create_list(request):
         if form.is_valid():
             form.save()
             messages.success(request, "List created successfully.")
-            return redirect('list_home')
+            return redirect('home')
         else:
-            return redirect('create_list')
+            messages.error(request, "ERROR: Could not create list.") 
+            return redirect('home')
     else:
-        form - ListForm()
+        form = ListForm()
         context = {
             "form": form,
         }
