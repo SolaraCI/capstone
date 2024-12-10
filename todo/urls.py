bdf72161c8e7a5759414c8398ca7a70abb3de8db
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-10 19:28:29 +0000

    add delete_item url

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("lists/<list_id>", views.view_list, name="view_list"),
     path("register/", views.register, name="register"),
     path("login/", views.login_page, name="login"),
+    path("delete_item/<str:name>/", views.delete_item, name="delete_item"),
 ]
