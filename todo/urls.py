492660fa50793ae79df1bd2a9d82ac1520b4ea4c
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-10 23:49:10 +0000

    upgrade to django-5.1.4, convert from function-based views to class-based views

--- todo/urls.py
+++ todo/urls.py
@@ -2,10 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.all_lists, name="home"),
-    path("create_list", views.create_list, name="create_list"),
-    path("lists/<list_id>", views.view_list, name="view_list"),
-    path("register/", views.register, name="register"),
-    path("login/", views.login_page, name="login"),
-    path("delete_item/<str:name>/", views.delete_item, name="delete_item"),
+    path("", views.OverView.as_view(), name='home'),
+    path("create_list", views.CreateListFormView.create_list, name="create_list"),
+    path("lists/<list_id>", views.SingleListView.view_list, name="view_list"),
+    path("register/", views.RegisterFormView.register, name="register"),
+    path("login/", views.LoginFormView.login_page, name="login"),
+    # path("delete_item/<str:name>/", views.delete_item, name="delete_item"),
 ]
