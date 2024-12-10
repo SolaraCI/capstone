ea0f8b5647b80763be749b102fd27c8399c97815
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-10 13:11:29 +0000

    add register view for user registration

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from .models import List, Item
 from .forms import ListForm, ItemForm
 from django.forms import modelform_factory
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -48,3 +50,32 @@ def create_list(request):
             "form": form,
         }
         return render(request, 'todo/create_list.html', context)
+    
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(password) < 4:
+            messages.error(request, 'Password must be at least 4 characters')
+            return redirect('register')
+        
+        get_all_user_names = User.objects.filter(username=username)
+        if get_all_user_names:
+            messages.error(request, 'Username must be unique. Please choose another.')
+            return redirect('register')
+        
+        get_all_user_emails = User.objects.filter(email=email)
+        if get_all_user_emails:
+            messages.error(request, 'Email address already in use. Please use another.')
+            return redirect('register')
+
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
+    return render(request,'todo/register.html', {})
+
+
+
+
