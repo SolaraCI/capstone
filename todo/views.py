8cc68871aaa43fb40f41a3a40c014e171b490109
Solara <fbp3sgxt@students.codeinstitute.net>
2024-12-12 01:23:03 +0000

    update delete_item function to handle items with identical names

--- todo/views.py
+++ todo/views.py
@@ -26,13 +26,21 @@ class SingleListView(ListView):
     def view_list(request, list_id):
         global current_list_id
         global todo_list
+        global item_sig_list
+        global item_names
+        global item_ids
         current_list_id = list_id
         todo_list = get_object_or_404(List, id=current_list_id)
+        item_sig_list = todo_list.items.values_list('name', 'id', named=True)
+        item_name_list = todo_list.items.values_list('name', flat=True)
+        item_id_list = todo_list.items.values_list('id', flat=True)
+
         
         print("-----------------------")
         print(f"current_list_id: { current_list_id }")
         print(f"todo_list: { todo_list }")
         print(f"parent list: { todo_list.items.values_list('parent_list', flat=True) }")
+        print(f"item_sig_list: { item_sig_list }")
         print(f"request: {request}")
         print("-----------------------")
 
@@ -45,7 +53,7 @@ class SingleListView(ListView):
         context = {
             'current_list_id' : current_list_id,
             'list': todo_list,
-            'items': todo_list.items,
+            'item_sig_list': todo_list.items.values_list('name', 'id', named=True),
             'item_names': todo_list.items.values_list('name', flat=True),
         }
 
@@ -53,9 +61,9 @@ class SingleListView(ListView):
     
 
     
-    def delete_item(request, current_list_id, name):
+    def delete_item(request, current_list_id, name, id):
         print(f"Delete item called with list_id: {current_list_id}, name: {name}")
-        item_to_delete = get_object_or_404(Item, name=name, parent_list_id=current_list_id)
+        item_to_delete = get_object_or_404(Item, name=name, parent_list_id=current_list_id, id=id)
         print(f"item_to_delete.id: {item_to_delete.id}")
         item_to_delete.delete()
         print(f"Deleted item: {name} from list {current_list_id}")
